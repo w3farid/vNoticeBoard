4592e494f2c2e5adc166be70a29241f136eef2e6
Farid <w3farid@gmail.com>
2017-09-29 14:48:55 +0600

    initial commit

---
+++ src/main/java/com/vNoticeBoard/VNoticeBoardApplication.java
@@ -0,0 +1,12 @@
+package com.vNoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VNoticeBoardApplication.class, args);
+	}
+}

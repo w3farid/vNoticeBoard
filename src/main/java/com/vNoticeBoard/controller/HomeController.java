d213e2ebc77f73cc2555ed67860033930b06d209
Farid <w3farid@gmail.com>
2017-09-30 11:43:39 +0600

    notice board create, edit completed

--- src/main/java/com/vNoticeBoard/controller/HomeController.java
+++ src/main/java/com/vNoticeBoard/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.vNoticeBoard.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String showHome() {
-		
+		LOGGER.debug("Rendaring home page.");
 		return "home";
 		
 	}
